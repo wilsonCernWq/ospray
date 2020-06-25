4e4e130d4c345e1da4f94227b566014b96602e42
Qi Wu <qi1.wu@intel.com>
2020-06-24 22:24:23 -0700

    [vscode] add task script to build deps

--- premake/mcs.anl.gov/premake.voxel.mcs.anl.gov.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/homes/qadwu/work/projects/ospray/premake.sh  --mpi -i /homes/qadwu/work/builds/voxel/ospray/install/ -o /homes/qadwu/work/builds/voxel/ospray/build/ --gcc --embree-dir "/homes/qadwu/work/softwares/embree-3.2.0.x86_64.linux" --tbb-dir "/homes/qadwu/work/softwares/tbb2018_20171205oss" --ispc-dir "/homes/qadwu/work/softwares/ispc-v1.9.2-linux" --icc-dir "" --gcc-dir "/soft/apps/packages/gcc/gcc-6.2.0/bin" --cmake-dir "cmake"
